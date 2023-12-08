13659f65abb1bb4daf2c62e9a7962d055c22c3b6
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2023-12-08 11:30:29 -0800

    Initial testing of deploy.sh for setting up new machines as miners

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Install Docker prerequisites
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install npm
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# Install PM2
+sudo npm install -g pm2
+
+# Clone cortex.t repository
+git clone https://github.com/corcel-api/cortex.t.git
+cd cortex.t
+
+# Install cortex.t dependencies
+pip install -e .
+
+cd ..
+
+# Clone subtensor repository
+git clone https://github.com/opentensor/subtensor.git
+
+# Start subtensor using Docker Compose
+cd subtensor
+sudo docker-compose up -d
+
+cd ..
+
+# Clone bittensor_auto_register_script repository
+git clone https://github.com/PhoenixBeaudry/bittensor_utils.git
+
+echo "Setup completed!"
