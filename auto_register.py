d6c0a9b56b01325fa81591cef1a93233db36a346
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2023-12-09 22:25:22 -0800

    Removed low UID sniping, not necessary, for now

--- auto_register.py
+++ auto_register.py
@@ -167,20 +167,10 @@ wallets = [
     {'wallet_name': 'coldwallet', 'wallet_hotkey': 'hotwallet', 'id': "1", 'port': '8098'}
 ]
 subnet_id = 18 # Hard coded subnet 18
-uid_snipe = True
-uid_snipe_threshold = 80
 
 # Make sure you have OPENAI_API_KEY and BT_COLD_PW_WALLETNAME in a .env file.
 if __name__ == "__main__":
     while True:
-        # If trying to get low UIDs, check availability
-        low_uid_available = False
-        if(uid_snipe):
-            endangered_uids = get_endangered_uids(subnet_id)
-            # Wait for low UID probability to be worth it.
-            if(endangered_uids[0][0] < uid_snipe_threshold and endangered_uids[1][0] < uid_snipe_threshold):
-                low_uid_available = True
-
         for wallet in wallets:
             wallet_config = get_wallet_config(wallet['wallet_name'], wallet['wallet_hotkey'], subnet_id)
 
@@ -191,18 +181,9 @@ if __name__ == "__main__":
             if(not is_registered):
                 print(f"Wallet: {wallet['id']} is not registered.")
                 
-                if(uid_snipe and low_uid_available):
-                    # Try register
-                    print("Low UID available.")
-                    print(f"Attempting register Wallet {wallet['id']}... ")
-                    register_result = register_wallet(wallet_config)
-                elif(uid_snipe and not low_uid_available):
-                    print("No low UIDs available. Waiting...")
-                    register_result = False
-                else:
-                    # Try register
-                    print(f"Attempting register Wallet {wallet['id']}... ")
-                    register_result = register_wallet(wallet_config)
+                # Try register
+                print(f"Attempting register Wallet {wallet['id']}... ")
+                register_result = register_wallet(wallet_config)
 
                 # Registration succeeded, start a miner
                 if (register_result == True):
