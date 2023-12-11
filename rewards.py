efe0ea5bfd7080fd9780d008f869c6f011d10613
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2023-12-10 17:26:24 -0800

    rewards.py tallys up rewards across wallets in wallets.json

--- rewards.py
+++ rewards.py
@@ -1,10 +1,31 @@
 import bittensor as bt
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 bt.subtensor.add_args(parser)
 config = bt.config(parser)
 subtensor = bt.subtensor(config=config)
+
+# Gets all the relevant Bittensor information on a specific wallet and subnet
+def get_wallet_config(wallet_name, wallet_hotkey, subnet_id):
+    parser = argparse.ArgumentParser()
+    bt.subtensor.add_args(parser)
+    bt.logging.add_args(parser)
+    bt.wallet.add_args(parser)
+    config = bt.config(parser)
+    config.wallet.name = wallet_name
+    config.wallet.hotkey = wallet_hotkey
+    wallet = bt.wallet(config=config)
+    subtensor = bt.subtensor(config=config)
+    metagraph = subtensor.metagraph(subnet_id)
+    wallet_config ={
+        'wallet': wallet,
+        'subnet_id': subnet_id,
+        'subtensor': subtensor,
+        'metagraph': metagraph
+    }
+    return wallet_config
 
 # Returns the current percent emissions of the given subnet.
 def get_subnet_weight(subnet_id):
@@ -20,6 +41,7 @@ def get_wallet_uid(hotkey, subnet_id):
             return uid
         uid += 1
     print("Wallet is not registered on this subnet.")
+
     return None
 
 # Returns the amount of TAO a hotkey is making per day
@@ -33,7 +55,20 @@ def get_rewards_per_day(hotkey, subnet_id):
 
 
 if __name__ == "__main__":
-    # Wallet Hot Key
-    wallet_hotkey = "ss58 address"
-    subnet_id = 1
-    print(get_rewards_per_day(wallet_hotkey, subnet_id))
+    # Open the JSON file and load its content into a Python object
+    with open('wallets.json', 'r') as file:
+        wallets = json.load(file)
+
+    subnet_id = 18
+
+    total_tao = 0
+
+    for wallet in wallets:
+        wallet_config = get_wallet_config(wallet['wallet_name'], wallet['wallet_hotkey'], subnet_id)
+        # Wallet Hot Key
+        wallet_hotkey = wallet_config['wallet'].get_hotkey().ss58_address
+        wallet_reward = get_rewards_per_day(wallet_hotkey, subnet_id)
+        print(f"Rewards for wallet {wallet['id']}: {wallet_reward}")
+        total_tao += wallet_reward
+
+    print(f"Total Daily Rewards: {total_tao}")
